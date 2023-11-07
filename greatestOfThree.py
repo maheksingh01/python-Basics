280e00dd65a86c3dd305efc39f7e7ed68716851b
Mahek <mahek.2002.singh@gmail.com>
2023-11-07 20:10:30 +0530

    greatest of three

---
+++ greatestOfThree.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the Number : "))
+b = int(input("Enter the Number : "))
+c = int(input("Enter the Number : "))
+
+if(a>b and a>c):
+    print("a is greatest")
+elif(b>a and b>c):
+    print("b is greatest")
+else:
+    print("c is greatest")
