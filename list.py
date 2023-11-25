9e1085efbdca43223d7ee436f2675eae32a59deb
Mahek <mahek.2002.singh@gmail.com>
2023-11-25 21:13:37 +0530

    list

---
+++ list.py
@@ -0,0 +1,32 @@
+#original list
+L=[1,2,3]
+
+#append function
+L.append(4)
+print (L)
+
+#second list
+L1 = [45,18,7,2]
+print(L1)
+
+#sort function
+L1.sort() 
+
+#pop function
+L.pop(1)
+
+print(L)
+print(L1)
+
+#third list
+L2=[12,1,2,1,2,1,2]
+
+#count function
+co = L2.count(1)
+print(co)
+
+#reverse function
+L2.reverse()
+print(L2)
+
+#list slicing
