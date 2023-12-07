87e01ca660184e277fbb716ff74a1392d53d8561
Mahek <mahek.2002.singh@gmail.com>
2023-12-07 16:59:15 +0530

    pattern 8

---
+++ pattern7.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the value of n : "))
+num = 1
+for i in range (0,n):
+    print(" " * (n-i), end=" ")
+    for j in range (0, i+1):
+        print(num, end=" ")
+        num+=1
+    num = 1
+    print("")
