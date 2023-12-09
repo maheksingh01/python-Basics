c6512904eb4b27013cd0d265abb8648c48de2526
Mahek <mahek.2002.singh@gmail.com>
2023-12-09 17:03:39 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,25 @@
+
+#original sets
+s1 = {1,2,3,4}
+s2 = {4,8,12}
+s3 = {3,6,9}
+
+print(s1)
+print(s2)
+print(s3)
+
+#union
+s4 = s1 | s2
+print (s4)
+
+#intersection
+s5 = s1 & s3
+print(s5)
+
+#difference
+s6 = s1 - s2
+print(s6)
+
+#symmetric difference
+s7 = s1 ^ s2
+print(s7)
