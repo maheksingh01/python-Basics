33c563f4599c70165509585e89717624db685c15
Mahek Singh <mahek.2002.singh@gmail.com>
2024-10-18 08:35:01 +0530

    adding: demo

---
+++ FirstDemo.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+# comments in python
+
+a = 3
+print(a)
+
+Str = "My name is Singh"
+print(Str)
+
+b, c, d = 5, 6.4, "Great"
+print(b)
+print(c)
+print(d)
+
+# concatination in python
+print("{} {}".format("Value is", b))
+
+#type of variable
+print(type(c))
