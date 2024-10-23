67ffc6c94b84288f4ddc1c76a210ab9cb1c76c8a
Mahek Singh <mahek.2002.singh@gmail.com>
2024-10-23 09:04:52 +0530

    adding: palindrome using recursion

---
+++ palindromeUsingRecursion.py
@@ -0,0 +1,12 @@
+def palin (text):
+    if(len(text) == 1):
+        print("palindrome")
+    else:
+        if(text[0]==text[-1]):
+            palin(text[1:-1])
+        else:
+            print("not a pallindrome")
+
+
+text = "Enter the text : "
+palin(text)
