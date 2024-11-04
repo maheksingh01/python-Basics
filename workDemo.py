0f5932fe36e7d2ab340c89778e9c44ad2fbe43c2
Mahek Singh <mahek.2002.singh@gmail.com>
2024-11-04 09:16:49 +0530

    adding: updated inplict wait time

--- workDemo.py
+++ workDemo.py
@@ -30,6 +30,18 @@ for result in results:
 
 driver.find_element(By.CSS_SELECTOR,"img[alt='Cart']").click()
 driver.find_element(By.XPATH,"//button[text()='PROCEED TO CHECKOUT']").click()
+
+# sum validation
+prices = driver.find_elements(By.CSS_SELECTOR,"tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+    
+print (sum)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+
+assert sum == totalAmount
+
 driver.find_element(By.CSS_SELECTOR,".promocode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR,".promoBtn").click()
 print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
