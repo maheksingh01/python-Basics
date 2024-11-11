5db0992d142a86f86563db9794d77ccd1c3ec49c
Mahek Singh <mahek.2002.singh@gmail.com>
2024-11-11 08:11:04 +0530

    adding: demo browser

--- Udemy/code-Part1/demoBrowser.py
+++ Udemy/code-Part1/demoBrowser.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 #Through Selenium test we need to invoke the executable file which will then invoke actual browser
 #driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
 #driver=webdriver.Firefox(executable_path="C:\\geckodriver.exe")
-driver = webdriver.Ie(executable_path="C:\\IEDriverServer.exe")
+# driver = webdriver.Ie(executable_path="C:\\IEDriverServer.exe")
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/")  #get method to hit url on  browser
 
